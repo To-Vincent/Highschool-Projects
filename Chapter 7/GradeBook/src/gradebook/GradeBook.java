aceb9880b7e5b09d5dda493565d3e015e2de6dea
Vincent <61031082+YoRHa-Whiteness@users.noreply.github.com>
2020-10-15 16:41:07 -0400

    Add files via upload

---
+++ Chapter 7/GradeBook/src/gradebook/GradeBook.java
@@ -0,0 +1,16 @@
+package gradebook;
+
+public class GradeBook {
+    
+    public static void main(String[] args) {
+        Scores s = new Scores();
+        
+        s.setNames();
+        
+        s.setScores();
+        
+        s.getScores();
+        
+    }
+
+}
